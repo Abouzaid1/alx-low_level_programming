308d503b478e15e888ab3a5450b4655eebfae573
Abouzaid1 <am5715073@gmail.com>
2023-11-16 15:40:52 +0200

    adding the linked list code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
  * print_list - Prints all the elements of a list_t list.
  * @h: The head of the list.
@@ -10,20 +9,19 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
-
-    while (h)
-    {
-        if (h->str == NULL)
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%d] %s\n", h->len, h->str);
-        }
-        count++;
-        h = h->next;
-    }
-    return (count);
+	size_t count = 0;
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
