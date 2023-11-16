ef0edf60cf3d1ca51b8a2e162cd1a26b64017917
Abouzaid1 <am5715073@gmail.com>
2023-11-16 14:10:14 +0200

    adding the linked list code

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -9,11 +10,11 @@
  * Description: singly linked list node structure
  */
 typedef struct list_s
-	{
-		char *str;
-		unsigned int len;
-		struct list_s *next;
-	} list_t;
-
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 size_t print_list(const list_t *h);
+
 #endif
