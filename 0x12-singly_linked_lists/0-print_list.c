cda5dadb8efc967bd0fa8c25a680cebdf2915bf5
Abouzaid <am5715073@gmail.com>
2023-11-16 08:16:57 -0800

    changes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,6 +3,21 @@
 #include <stdio.h>
 #include "lists.h"
 /**
+* _strlen - function that returns the length of a string.
+* @s : s is a character
+* Return: value is i
+*/
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
 *print_list - Prints all the elements of a list_t list.
 *@h: The head of the list.
 *Return: The number of nodes.
