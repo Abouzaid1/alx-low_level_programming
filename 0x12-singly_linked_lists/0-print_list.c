bf783670b10ecd35fc0c16c3709e3e3755c9b757
Abouzaid1 <am5715073@gmail.com>
2023-11-16 13:54:57 +0200

    adding the linked list code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,25 +5,25 @@
 
 /**
  * print_list - check Holberton School students.
- * @list: name of the list
+ * @h: name of the list
  * Return: the number of nodes.
  */
-size_t print_list(const list_t *list)
+size_t print_list(const list_t *h)
 {
 	int count = 0;
 
-	while (list)
+	while (h)
 	{
-		if (list->str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-		printf("[%d] %s\n", list->len, list->str);
+		printf("[%d] %s\n", h->len, h->str);
 		}
 		count++;
-		list = list->next;
+		h = h->next;
 	}
 	return (count);
 }
