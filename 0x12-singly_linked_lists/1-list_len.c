b2afb88ddefa7e6e53efdb6d7f43bc589a26a451
Abouzaid1 <am5715073@gmail.com>
2023-11-16 15:10:38 +0200

    adding the linked list code

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,21 +2,6 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-/**
- * _strlen - function that returns the length of a string.
- * @s : s is a character
- * Return: value is i
- */
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
 
 /**
  * list-len - length of the list
